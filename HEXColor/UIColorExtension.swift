60ddff8d9844a9398963bb899ae47d9819d2cee8
Hariton Batkov <yanker90@gmail.com>
2017-09-16 12:20:11 +0300

    added objc modifiers

--- HEXColor/UIColorExtension.swift
+++ HEXColor/UIColorExtension.swift
@@ -20,7 +20,7 @@ public enum UIColorInputError : Error {
     unableToOutputHexStringForWideDisplayColor
 }
 
-extension UIColor {
+@objc extension UIColor {
     /**
      The shorthand three-digit hexadecimal representation of color.
      #RGB defines to the color #RRGGBB.
@@ -28,7 +28,7 @@ extension UIColor {
      - parameter hex3: Three-digit hexadecimal value.
      - parameter alpha: 0.0 - 1.0. The default is 1.0.
      */
-    public convenience init(hex3: UInt16, alpha: CGFloat = 1) {
+    @objc public convenience init(hex3: UInt16, alpha: CGFloat = 1) {
         let divisor = CGFloat(15)
         let red     = CGFloat((hex3 & 0xF00) >> 8) / divisor
         let green   = CGFloat((hex3 & 0x0F0) >> 4) / divisor
@@ -42,7 +42,7 @@ extension UIColor {
      
      - parameter hex4: Four-digit hexadecimal value.
      */
-    public convenience init(hex4: UInt16) {
+    @objc public convenience init(hex4: UInt16) {
         let divisor = CGFloat(15)
         let red     = CGFloat((hex4 & 0xF000) >> 12) / divisor
         let green   = CGFloat((hex4 & 0x0F00) >>  8) / divisor
@@ -56,7 +56,7 @@ extension UIColor {
      
      - parameter hex6: Six-digit hexadecimal value.
      */
-    public convenience init(hex6: UInt32, alpha: CGFloat = 1) {
+    @objc public convenience init(hex6: UInt32, alpha: CGFloat = 1) {
         let divisor = CGFloat(255)
         let red     = CGFloat((hex6 & 0xFF0000) >> 16) / divisor
         let green   = CGFloat((hex6 & 0x00FF00) >>  8) / divisor
@@ -69,7 +69,7 @@ extension UIColor {
      
      - parameter hex8: Eight-digit hexadecimal value.
      */
-    public convenience init(hex8: UInt32) {
+    @objc public convenience init(hex8: UInt32) {
         let divisor = CGFloat(255)
         let red     = CGFloat((hex8 & 0xFF000000) >> 24) / divisor
         let green   = CGFloat((hex8 & 0x00FF0000) >> 16) / divisor
@@ -83,7 +83,7 @@ extension UIColor {
      
      - parameter rgba: String value.
      */
-    public convenience init(rgba_throws rgba: String) throws {
+    @objc public convenience init(rgba_throws rgba: String) throws {
         guard rgba.hasPrefix("#") else {
             throw UIColorInputError.missingHashMarkAsPrefix
         }
@@ -114,7 +114,7 @@ extension UIColor {
      
      - parameter rgba: String value.
      */
-    public convenience init(_ rgba: String, defaultColor: UIColor = UIColor.clear) {
+    @objc public convenience init(_ rgba: String, defaultColor: UIColor = UIColor.clear) {
         guard let color = try? UIColor(rgba_throws: rgba) else {
             self.init(cgColor: defaultColor.cgColor)
             return
@@ -127,7 +127,7 @@ extension UIColor {
      
      - parameter includeAlpha: Whether the alpha should be included.
      */
-    public func hexStringThrows(_ includeAlpha: Bool = true) throws -> String  {
+    @objc public func hexStringThrows(_ includeAlpha: Bool = true) throws -> String  {
         var r: CGFloat = 0
         var g: CGFloat = 0
         var b: CGFloat = 0
@@ -150,7 +150,7 @@ extension UIColor {
      
      - parameter includeAlpha: Whether the alpha should be included.
      */
-    public func hexString(_ includeAlpha: Bool = true) -> String  {
+    @objc public func hexString(_ includeAlpha: Bool = true) -> String  {
         guard let hexString = try? hexStringThrows(includeAlpha) else {
             return ""
         }
